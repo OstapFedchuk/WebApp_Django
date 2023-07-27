08e25c98a15d290b94d993643fedd6f1daa7cd88
OstapFedchuk <ostapit12345@gmail.com>
2023-07-27 09:09:03 +0200

    Data una sistematina ai template

--- myblog/urls.py
+++ myblog/urls.py
@@ -3,4 +3,12 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("about", views.about, name="about"),
+    path("contact", views.contact, name="contact"),
+    path("info", views.info, name="info"),
+    path("gitstatus", views.gitstatus, name="gitstatus"),
+    path("login", views.login, name="login"),
+    path("register", views.register, name="register"),
+    path("", views.logout, name="logout"),
+    path("recovery", views.recovery, name="recovery")
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -6,4 +6,31 @@ from .models import Signup, Contact
 # Create your views here.
 def index(request):
     global_username = "Giovanni"
-    return render(request, "index.html", {"global_username": global_username})
+    return render(request, "index.html", {"global_username": global_username}) #locals()
+
+def about(request):
+    global_username = "Giovanni"
+    return render(request, "about.html", {"global_username": global_username})
+
+def contact(request):
+    global_username = "Giovanni"
+    return render(request, "contact.html", {"global_username": global_username})
+
+def info(request):
+    global_username = "Giovanni"
+    return render(request, "info.html", {"global_username": global_username})
+
+def gitstatus(request):
+    return render(request, "gitstatus.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def register(request):
+    return render(request, "register.html")
+
+def logout(request):
+    return render(request, "index.html")
+
+def recovery(request):
+    return render(request, "recovery.html")
